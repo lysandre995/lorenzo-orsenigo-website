679eaff5b45147b46fd2fb3861b1a7cd8ba30381
lysandre995 <alessandro.alfano@linuxmail.org>
2023-04-12 15:36:50 +0200

    chore: continuing event component styling

--- src/app/components/events/event/event.component.ts
+++ src/app/components/events/event/event.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faTicket } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-event',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./event.component.css']
 })
 export class EventComponent {
-
+  public faTicket = faTicket;
 }
