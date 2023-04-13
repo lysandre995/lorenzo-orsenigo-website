29372dfe434c3c24599fbbe80b57e2ba4ef64116
lysandre995 <alessandro.alfano@linuxmail.org>
2023-04-13 09:24:24 +0200

    chore: page events fixes

--- src/app/components/events/event/event.component.ts
+++ src/app/components/events/event/event.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { EventInteface } from "../../../interfaces/event.inteface";
 import { EventParsedInterface } from "../../../interfaces/event-parsed.interface";
+import { constants } from "../../../constants";
 
 @Component({
   selector: 'app-event',
@@ -18,4 +19,27 @@ export class EventComponent {
     description: '',
     coverUrl: ''
   };
+
+  get day(): string {
+    return this.twoDigitsString(this.event.date.getDate());
+  }
+
+  get month(): string {
+    return constants.months[this.event.date.getMonth()];
+  }
+
+  get hours(): string {
+    return this.twoDigitsString(this.event.date.getHours());
+  }
+  get minutes(): string {
+    return this.twoDigitsString(this.event.date.getMinutes());
+  }
+
+  private twoDigitsString(n: number) {
+    if (Math.floor(n / 10) === 0) {
+      return '0' + String(n);
+    } else {
+      return String(n);
+    }
+  }
 }
---
+++ src/app/constants.ts
@@ -0,0 +1,16 @@
+export const constants = {
+  months: [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
+}
